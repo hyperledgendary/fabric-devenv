4fc2d1f526d376704d1c1e4d56095778821f306d
James Taylor <jamest@uk.ibm.com>
2021-06-28 14:06:01 +0100

    Use Bento box
    There are occasionally problems adding/updating the ubuntu/bionic64 box so trying
    equivalent Bento box instead…
    
    https://www.vagrantup.com/docs/boxes
    
    “If you encounter issues with [Canonical] boxes, please try the Bento boxes instead.”
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- provision-disk.sh
+++ provision-disk.sh
@@ -5,8 +5,8 @@ set -o pipefail
 
 if ! grep -Fxq "/dev/vagrant/home /home ext4 defaults 0 0" /etc/fstab; then
   # Add some disk space for opt and home directories
-  pvcreate /dev/sdc
-  vgcreate vagrant /dev/sdc
+  pvcreate /dev/sdb
+  vgcreate vagrant /dev/sdb
   lvcreate -L 5G -n opt vagrant
   lvcreate -L 5G -n home vagrant
   mkfs -t ext4 /dev/vagrant/opt
